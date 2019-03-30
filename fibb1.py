9c8735b096c0d5f3fad58a16cb79a86c85fa4612
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro-2.local>
2019-03-30 13:56:46 -0400

    first python attempt

--- fibb1.py
+++ fibb1.py
@@ -1,1 +1,17 @@
+i = 0
+j = 1
+evenNumbers = []
+sum = 0
 
+while sum<4000000:
+	sum=i+j
+	i=j
+	j=sum
+	if j%2==0:
+		evenNumbers.append(j)
+
+sumOfEven = 0
+for x in evenNumbers:
+	sumOfEven = sumOfEven+x
+
+print( sumOfEven)	
