cdb3eb2c2d1e779a767155dd4da35daff34c393b
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro-2.local>
2019-04-01 16:27:08 -0400

    2 attempts of counting first Sunday

--- sundays.py
+++ sundays.py
@@ -1,1 +1,72 @@
+# Start at 1/1/1901 (Tuesday)
 
+countOfSundays = 0;
+year = 1901
+dayOfWeek = 2
+
+def month(startingDayOfWeek):
+	if startingDayOfWeek == 0:
+		return 1
+	return 0
+
+def weekDay(dayOfWeek):
+	return dayOfWeek % 7
+
+def addDayforLeapYear(year):
+	if year%4 == 0 :
+		return 1
+	return 0
+
+while year < 2001:
+	# Jan
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 31)
+	# print(dayOfWeek)
+	# Feb
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 28 + addDayforLeapYear(year))
+	# print(dayOfWeek)
+	# March
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 31)
+	# print(dayOfWeek)
+	# April
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 30)
+	# print(dayOfWeek)
+	# May
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 31)
+	# print(dayOfWeek)
+	# June
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 30)
+	# print(dayOfWeek)
+	# July
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 31)
+	# print(dayOfWeek)
+	# Aug
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 31)
+	# print(dayOfWeek)
+	#Sept
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 30)
+	# print(dayOfWeek)
+	# Oct
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 31)
+	# print(dayOfWeek)
+	# Nov
+	countOfSundays = countOfSundays + month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 30)
+	# print(dayOfWeek)
+	# Dec
+	countOfSundays = countOfSundays +  month(dayOfWeek)
+	dayOfWeek = weekDay(dayOfWeek + 31)
+	# print(dayOfWeek)
+
+	year = year + 1
+
+print("Sunday " , countOfSundays)
